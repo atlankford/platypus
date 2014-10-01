603045a8bc3bceb4b1d77ceab4ef878891af4837
atlankford <atlankford@gmail.com>
2014-10-01 19:03:05 -0400

    major update

---
+++ manifest.php
@@ -0,0 +1,24 @@
+
+CACHE MANIFEST
+
+CACHE:
+./index.html
+./js/location.js
+./js/firebase.js
+./bower_components/framework7/dist/css/framework7.min.css
+./bower_components/framework7/dist/css/framework7.themes.min.css
+./css/site.css
+./js/vendor/geofire.min.js
+./bower_components/framework7/dist/js/framework7.min.js
+./js/location.js
+../../dist/js/framework7.min.js
+<br />
+<b>Warning</b>:  md5_file(../../dist/js/framework7.min.js) [<a href='function.md5-file'>function.md5-file</a>]: failed to open stream: No such file or directory in <b>/home1/lankford/public_html/weather/manifest.php</b> on line <b>22</b><br />
+../../dist/css/framework7.min.css
+<br />
+<b>Warning</b>:  md5_file(../../dist/css/framework7.min.css) [<a href='function.md5-file'>function.md5-file</a>]: failed to open stream: No such file or directory in <b>/home1/lankford/public_html/weather/manifest.php</b> on line <b>22</b><br />
+
+NETWORK:
+*
+
+# Hash Version: 407ccef2c4e8130051e6cf57261f58b9
